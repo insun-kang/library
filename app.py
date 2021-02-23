9bbf2c78d5854a4334b6d5fe02a8377e7044d539
인선 강 <sunink1@naver.com>
2021-02-23 20:38:37 +0900

    fasd

--- app.py
+++ app.py
@@ -36,12 +36,11 @@ def register():
 
 if __name__ == "__main__":
     #데이터베이스---------
-    basedir = os.path.abspath(os.path.dirname(__file__)) #현재 파일이 있는 디렉토리 절대 경로
-    dbfile = os.path.join(basedir, 'db.sqlite') #데이터베이스 파일을 만든다
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #사용자에게 정보 전달완료하면 teadown. 그 때마다 커밋=DB반영
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #추가 메모리를 사용하므로 꺼둔다
+ 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/library?charset=utf8'
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = 'manyrandombyte'
 
 #    db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
     db.init_app(app) #app설정값 초기화
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
 
 class User(db.Model): #데이터 모델을 나타내는 객체 선언
     __tablename__ = 'user_table' #테이블 이름
-    
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(32), unique=True, nullable=False)
     username = db.Column(db.String(32), unique=True, nullable=False)
