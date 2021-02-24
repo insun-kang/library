7c2a9952dc9e6f7d33749db444b66653d88a1c0a
인선 강 <sunink1@naver.com>
2021-02-24 17:02:40 +0900

    회원가입 구현

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os #디렉토리 절대 경로
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from models import db
 from models import User
@@ -9,6 +9,26 @@ app = Flask(__name__)
 def hello():
 	return render_template('login.html')
 
+@app.route('/login', methods=['GET','POST'])  
+def login():
+    if request.method =='GET':
+        return render_template('login.html')
+    else:
+        email=request.form.get('email')
+        password=request.form.get('password')
+        try:
+            data=User.query.filter_by(email=em, password=pw).first() 
+
+            if data is not None:
+                session['logged_in'] = True
+                return redirect(url_for('main'))
+            else:
+                return 'Dont Login'
+        except:
+            return "Dont Login"
+
+
+
 @app.route('/register', methods=['GET','POST']) #GET(정보보기), POST(정보수정) 메서드 허용
 def register():
     if request.method == 'GET':
@@ -17,7 +37,7 @@ def register():
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        password_2 = request.form.get('password')
+        password_2 = request.form.get('re_password')
 
         if not(username and email and password and password_2):
             return "입력되지 않은 정보가 있습니다"
@@ -35,16 +55,15 @@ def register():
         return redirect('/')
 
 if __name__ == "__main__":
-    #데이터베이스---------
- 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/library?charset=utf8'
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/library'
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.secret_key = 'manyrandombyte'
 
-#    db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
-    db.init_app(app) #app설정값 초기화
-    db.app = app #Models.py에서 db를 가져와서 db.app에 app을 명시적으로 넣는다
-    db.create_all() #DB생성
+
+    db.init_app(app) 
+    db.app = app
+    db.create_all() 
 
     app.run(host="127.0.0.1", port=5000, debug=True)
--- models.py
+++ models.py
@@ -3,19 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy() #SQLAlchemy를 사용해 데이터베이스 저장
 
 class User(db.Model): #데이터 모델을 나타내는 객체 선언
-    __tablename__ = 'user_table' #테이블 이름
+    __tablename__ = 'usertable' #테이블 이름
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(32), unique=True, nullable=False)
     username = db.Column(db.String(32), unique=True, nullable=False)
-    password = db.Column(db.String(8), nullable=False)
+    password = db.Column(db.String(32), nullable=False)
 
-    def __init__(self, email, password):
-        self.email = email
-        self.set_password(password)
-    
+
     def set_password(self, password):
         self.password = generate_password_hash(password)
  
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
