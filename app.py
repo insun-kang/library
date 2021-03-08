21b0f55e43d2fb649db439c4f1e8f5926b35b238
Ubuntu <azure@elice-kdt-ai-track-vm-racer-02.hqbktzrxcscu3ivrumcvgloqqd.syx.internal.cloudapp.net>
2021-03-08 04:30:23 +0000

    3권까지 대여

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ import os
 import datetime
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from models import db
 from models import User, Book, Bookrental, Comment
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-
-
 
 
 @app.route('/', methods=['GET','POST'])
@@ -17,6 +16,7 @@ def home():
 	    return render_template('index.html')
     else:
         return render_template('index.html')
+
 
 @app.route('/login', methods=['GET','POST'])  
 def login():
@@ -36,10 +36,12 @@ def login():
         else:
             return ''' <script> alert('비밀번호가 틀렸습니다' ); location.href="/login" </script> '''
 
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     return ''' <script> alert('로그아웃 되었습니다' ); location.href="/" </script> '''
+
 
 @app.route('/register', methods=['GET','POST']) 
 def register():
@@ -71,6 +73,7 @@ def register():
         return redirect('/')
 
 
+
 @app.route('/main', methods=['GET','POST']) 
 def main():
     books = Book.query.all()
@@ -88,11 +91,15 @@ def main():
         data_user = User.query.filter_by(email=session['logged_in']).first()
         data_rentalbook=Bookrental.query.filter_by(book_id=data_book.id).first()
 
+        
         if data_book.quantity<=0:
             return ''' <script> alert("대여할 수 없습니다"); location.href="/main" </script> '''
-
+        
+        elif db.session.query(Bookrental).filter(Bookrental.return_date.like('미반납')).count()>2:
+                return ''' <script> alert("최대 3권만 빌릴 수 있습니다"); location.href="/main" </script> '''
  
         else:
+            
             data_book.quantity -= 1
 
             bookrental= Bookrental(
@@ -108,11 +115,13 @@ def main():
             return ''' <script> alert("대여되었습니다"); location.href="/main" </script> '''
         return render_template('main.html', books=books)
 
+
 @app.route('/BookRental', methods=['GET','POST']) 
 def BookRental():
     data_user=User.query.filter_by(email=session['logged_in']).first()
     rentalbooks = Bookrental.query.filter_by(user_id=data_user.id)
     return render_template('BookRental.html', rentalbooks=rentalbooks)
+
 
 @app.route('/returnbook', methods=['GET','POST']) 
 def returnbook():
@@ -134,33 +143,40 @@ def returnbook():
 
     return render_template('returnbook.html', returnbooks=returnbooks)
 
+
 @app.route('/books/<int:book_id>/', methods=['GET','POST'])
 def books(book_id):
     book = Book.query.filter_by(id=book_id).first()
-    comments= Comment.query.filter_by(book_id=book_id)
+    comments= Comment.query.filter_by(book_id=book_id).all()
     
     if request.method == 'GET':
         return render_template('bookdescript.html', book=book, comments=comments)
     else:
 
         data_user = User.query.filter_by(email=session['logged_in']).first()
-        find_user = Comment.query.filter_by(user_id=data_user.id)
-        content=request.form['content']
-        rating=int(request.form['rating'])+1
+        find_user = Comment.query.filter_by(user_id=data_user.id, book_id=book.id).first()
 
-        comment= Comment(
-                book_id = book.id,
-                user_id = data_user.id,
-                content = content,
-                rating = rating,
-                create_date = datetime.date.today()
-            )
-        db.session.add(comment)
-        db.session.commit()
+        if find_user is None:
+            content=request.form['content']
+            rating=int(request.form['rating'])+1
 
-        return render_template('bookdescript.html', book=book, comments=comments)
-            
-        
+
+            comment= Comment(
+                    book_id = book.id,
+                    user_id = data_user.id,
+                    content = content,
+                    rating = rating,
+                    create_date = datetime.datetime.now()
+                )
+            db.session.add(comment)
+            db.session.commit()
+
+
+
+            return render_template('bookdescript.html', book=book, comments=comments)
+        else:
+            return ''' <script> alert("댓글은 한번만 입력 가능합니다"); location.href="/books/{}/" </script> '''.format(book.id)
+
 
 if __name__ == "__main__":
 
